8c1932354842db9a3b5ac95f4c582b71ca1e0e15
Travis Hawks <thawks14@gmail.com>
2026-05-03 13:17:54 -0500

    updated to try and connect to the docker kafka

--- spark_job/app/main.py
+++ spark_job/app/main.py
@@ -8,7 +8,15 @@ def create_spark_session() -> SparkSession:
         SparkSession: A PySpark spark session
     """
     # naming the session after the repo. Only running locally since its not a large prod project
-    return SparkSession.builder.appName("sports-streaming-pipeline").master("local[*]").getOrCreate()
+    return (
+        SparkSession.builder.appName("sports-streaming-pipeline")
+        .master("local[*]")
+        .config(
+            "spark.jars.packages",
+            "org.apache.spark:spark-sql-kafka-0-10_2.13:4.1.1",
+        )
+        .getOrCreate()
+    )
 
 
 def main() -> None:
@@ -16,6 +24,18 @@ def main() -> None:
     spark = create_spark_session()
 
     print(f"Spark started successfully. Spark version: {spark.version}")
+
+    # testing spark to read from the kafka docker
+    kafka_df = (
+        spark.read.format("kafka")
+        .option("kafka.bootstrap.servers", "localhost:9092")
+        .option("subscribe", "nba.game_scores.raw")
+        .option("startingOffsets", "earliest")
+        .load()
+    )
+
+    # actually not sure the exact meaning of this. a tutorial used it to test output. new to me.
+    kafka_df.selectExpr("CAST(value AS STRING) as message").show(truncate=False)
 
     # stop spark when done processing data
     spark.stop()
